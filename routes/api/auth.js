a0ea05a329014c4f31249c0e7c445d159cf22ad8
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-07-10 10:44:57 -0500

    auth and jwt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const Users = require('../../models/users')
 
 // Destructuring out the functions from express-validator
 const {
@@ -11,8 +12,8 @@ const {
 
 // A function for checking the headers of a request for a token
 const auth = require('../../middleware/auth');
-// Reference to the User schema
-const Users = require('../../models/users');
+
+
 
 // @route   GET api/auth
 // @desc    Authentication check route
@@ -22,8 +23,8 @@ const Users = require('../../models/users');
 router.get('/', auth, async (req, res) => {
     try {
         // Searching for a user by the value from the token and getting all info EXCEPT the password
-        const users = await Users.findById(req.users.id).select('-password');
-        res.json(users);
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
     } catch (err) {
         console.log(err.message);
         res.status(500).send('Server Error');
@@ -33,90 +34,67 @@ router.get('/', auth, async (req, res) => {
 // @route   POST api/auth
 // @desc    Authenticate user login
 // @access  Public
-router.post('/', 
-    // Passing an array to express-validator to check the req.body for validations
-    // https://express-validator.github.io/docs/
-    [
-        // Checking req.body for a email property and setting a message to respond with if there isnt one
-        check('email', 'Please include a valid email')
-            .isEmail(),
-        check('password', 'Password is required')
-            .exists(),
-        check('creditCard','Credit Card is required')
-            .exists()
-        
-    ],
-    // Setting async for proper asynchronus requests
-    async (req, res) => {
-        // Checking the result of the express-validator
-        const errors = validationResult(req);
-        // If there is an error, respond with it
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array()
-            });
-        }
-        // Since there was no error, destruture req.body for the values needed for the user
-        const {
-            email,
-            password
-        } = req.body;
-        // Setting the try block for the await sections
+
+// Passing an array to express-validator to check the req.body for validations
+// https://express-validator.github.io/docs/
+
+
+
+router.post('/login', async (req, res) => {
+
         try {
-            // Checking the database for an existing matching email
-            let users = await Users.findOne({
-                email
+
+            const verifyToken = jwt.verify(req.body.token, "SSEMNG$51423", {maxAge: 360000})
+            
+            const {data} = verifyToken
+
+            const login = await Users.findOne({
+                email: data.email
             })
-            // If there is no existing user then error as the credentials are invalid
-            if (!user) {
+            if (!login) {
                 return res.status(400).json({
                     errors: [{
-                        msg: 'Invalid Credentials'
+                        msg: "Email not found"
                     }]
                 });
             }
-            // Checking the stored encrypted password with the password provided (Returns a boolean)
-            // https://www.npmjs.com/package/express-validator
-            const isMatch = await bcrypt.compare(password, users.password);
+
+            const isMatch = await bcrypt.compare(req.body.password, login.password);
             // If isMatch is false error out as the password is incorrect
             if (!isMatch) {
                 return res.status(400).json({
                     errors: [{
-                        msg: 'Invalid Credentials'
+                        msg: 'Invalid Password'
                     }]
                 });
             }
-        
-            // Create a value to respond to the front end with
-            const payload = {
-                users: {
-                    id: users.id
-                }
-            }
-            // Creating a token to validate legitimate users without constantly checking the database
-            jwt.sign(
-                // Passing the value to encrypt
-                payload,
-                // Getting the encryption secret
-                process.env.jwtSecret, 
-                // Setting a length of time in milliseconds for the token to last for before requiring relogin
+
+            jwt.sign({
+                    data: {
+                        email: login.email,
+                        id: login._id,
+                    }
+                },
+
+                "SSEMNG$51423",
+
                 {
                     expiresIn: 360000
                 },
-                // Checking for errors then responding to the front end with the token for it to store.
+
                 (err, token) => {
                     if (err) throw err;
                     res.json({
                         token
                     });
                 }
-            );
-        } catch (err) {
-            // If any errors during the try that isn't accounted for get sent as a 500 Bad request
-            res.status(500).send('Server Error');
-        }
 
-    });
+            )
+
+        } catch (error) {
+            res.json(error)
+        }
+})
 
 
 module.exports = router;
